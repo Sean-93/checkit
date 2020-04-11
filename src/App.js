689431fc3715bc3c3e3ebd7572f40d165c135bb4
mekaleka <www.no-reply.com>
2020-04-11 12:19:48 -0700

    created auth0 files for user login authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Login from "./components/login";
-import SignUp from "./components/signup";
-import LandingPage from "./components/landingpage";
-import Post from "./components/post";
+import NavBar from "./components/Navbar";
+import { useAuth0 } from "./react-auth0-spa";
 
-const App = () => (
-  <>
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/landingpage" component={LandingPage} />
-          <Route exact path="/post" component={Post} />
-        </Switch>
-      </div>
-    </Router>
-  </>
-);
+function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+    </div>
+  );
+}
 
 export default App;
+
+
+
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import "./App.css";
+// import Login from "./components/login";
+// import SignUp from "./components/signup";
+// import LandingPage from "./components/landingpage";
+// import Post from "./components/post";
+
+// const App = () => (
+//   <>
+//     <Router>
+//       <div>
+//         <Switch>
+//           <Route exact path="/" component={Login} />
+//           <Route exact path="/signup" component={SignUp} />
+//           <Route exact path="/landingpage" component={LandingPage} />
+//           <Route exact path="/post" component={Post} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   </>
+// );
+
+// export default App;
 
