375caf127d4b96ef0a8476db5ceb118c37d9c59e
Sean Rooney <seandanielrooney@gmail.com>
2020-04-19 14:34:42 -0700

    deploying to Heroku

--- client/build/precache-manifest.b3877dc3d7fefc0a0d3113617d225bd0.js
+++ client/build/precache-manifest.b3877dc3d7fefc0a0d3113617d225bd0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a71897a67113fb3459fc21a2e9b06d0a",
+    "revision": "d49eac6785f4e12e3506d145b200851b",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "da506a7b4ad9497f9b6e",
+    "revision": "42993675353a07224031",
     "url": "/static/css/main.5c4b22c3.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f0c8dc64.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "da506a7b4ad9497f9b6e",
-    "url": "/static/js/main.a99e36f0.chunk.js"
+    "revision": "42993675353a07224031",
+    "url": "/static/js/main.fe7db6f9.chunk.js"
   },
   {
     "revision": "4e3fd3a753da25128add",
